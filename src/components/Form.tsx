913c16811451345096fb03d59583d7acb7f34813
Vadim <v_a_d@inbox.ru>
2024-03-07 14:20:31 +0300

    add Form component

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,6 +2,29 @@
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { validate } from '../utils.ts';
 
+type RequestBodyState = {
+  action: string;
+  params?: {
+    offset?: number;
+    limit?: number;
+    ids?: string[];
+    field?: string;
+    product?: string;
+    price?: number;
+    brand?: string;
+  }
+};
+
+type FormProps = {
+  setRequestBodyState: React.Dispatch<React.SetStateAction<RequestBodyState>>;
+};
+
+type Inputs = {
+  product: string;
+  price: string;
+  brand: string;
+}
+
 function Form({ setRequestBodyState }: FormProps) {
   const {
     register,
